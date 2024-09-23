cb3238c48ee88a04a36e452b6a396740b22b30f0
ZeppIine <kim0904min@gmail.com>
2024-09-23 16:28:11 +0900

    Initial commit

---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20180147/w24w03css/W24w03cssApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20180147.w24w03css
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w03cssApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24w03cssApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/ce/s20180147/w24w03css/W24w03cssApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20180147.w24w03css
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w03cssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
